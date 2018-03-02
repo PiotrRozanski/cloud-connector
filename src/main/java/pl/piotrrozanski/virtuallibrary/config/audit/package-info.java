a096b54ad864771c64c0648091429ec600feb575
Piotr Różański <87191@stud.uz.zgora.pl>
2018-03-02 01:12:02 +0100

    Generated with JHipster 4.14.0

---
+++ src/main/java/pl/piotrrozanski/virtuallibrary/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package pl.piotrrozanski.virtuallibrary.config.audit;
---
+++ src/main/java/pl/piotrrozanski/virtuallibrary/config/dbmigrations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MongoDB database migrations using MongoBee.
+ */
+package pl.piotrrozanski.virtuallibrary.config.dbmigrations;
